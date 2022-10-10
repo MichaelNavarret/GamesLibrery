d62281358f14fdd15d6efa27297815767e115648
Michael Navarrete <michael.07navarrete@gmail.com>
2022-10-10 19:00:49 -0300

    Agregar el poblado de tablas

---
+++ SCRIPTS_POBLADO_TABLAS.sql
@@ -0,0 +1,19 @@
+--Tabla Estado
+INSERT INTO ESTADO (idEstado, nombre ) VALUES ('TER', 'Terminado');
+INSERT INTO ESTADO (idEstado, nombre ) VALUES ('JUG', 'Jugando');
+INSERT INTO ESTADO (idEstado, nombre ) VALUES ('PSD', 'Pausado');
+INSERT INTO ESTADO (idEstado, nombre ) VALUES ('PDT', 'Pendiente');
+
+--TABLA TIPO
+INSERT INTO TIPO (idTipo, nombre ) VALUES ('HAS', 'Hack and Slash');
+INSERT INTO TIPO (idTipo, nombre ) VALUES ('RPG', 'RPG');
+INSERT INTO TIPO (idTipo, nombre ) VALUES ('JPG', 'JRPG');
+INSERT INTO TIPO (idTipo, nombre ) VALUES ('PTM', 'Plataformas');
+INSERT INTO TIPO (idTipo, nombre ) VALUES ('HRR', 'Horror');
+INSERT INTO TIPO (idTipo, nombre ) VALUES ('AVT', 'Aventuras');
+INSERT INTO TIPO (idTipo, nombre ) VALUES ('PZL', 'Puzles');
+INSERT INTO TIPO (idTipo, nombre ) VALUES ('TRP', 'Tactical RPG');
+INSERT INTO TIPO (idTipo, nombre ) VALUES ('MSO', 'Musó');
+INSERT INTO TIPO (idTipo, nombre ) VALUES ('CRR', 'Carreras');
+INSERT INTO TIPO (idTipo, nombre ) VALUES ('LCA', 'Lucha');
+
